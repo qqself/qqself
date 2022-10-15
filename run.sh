a9cf9698d51f61ccb3e91deed14501f8c1de5fc6
Artem Yarulin <artem.yarulin@kapteko.com>
2022-10-15 16:52:05 +0300

    aws-sync Automatically make a deployment to AppRunner

--- run.sh
+++ run.sh
@@ -31,12 +31,17 @@ test() {
 
 deploy() {
     log "Deploying api-sync"    
+    # Build new Docker image and push to our public AWS ECR registry
     repo="public.ecr.aws/z9w5n5h3"
     region="us-east-1"
     tag="$repo/qqself-api-sync:$VERSION"
-    docker build . --file api-sync/Dockerfile --tag $tag
+    docker build . --file api-sync/Dockerfile --tag "$tag"
     aws ecr-public get-login-password --region "$region" | docker login --username AWS --password-stdin "$repo"
-    docker push $tag
+    docker push "$tag"
+    # Initiate deployment: Get ARN of AWS AppRunner service and update it's config to point to the new Docker image tag
+    arn=$(aws apprunner list-services --query "ServiceSummaryList[?ServiceName=='qqself-api-sync'].ServiceArn" --output text)   
+    config="{\"ImageRepository\":{\"ImageIdentifier\":\"$tag\",\"ImageRepositoryType\":\"ECR_PUBLIC\"}}"
+    aws apprunner update-service --service-arn "$arn" --source-configuration "$config"
 }
 
 if [[ "$ACTION" == "build" ]]; then
