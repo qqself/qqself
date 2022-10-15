306ce36218d5e79833a3f066c2cbeb5ede03181e
Artem Yarulin <artem.yarulin@kapteko.com>
2022-10-15 10:58:35 +0300

    api-sync - Dockerfile and deployment script to AWS ECR (#5)

--- api-sync/src/main.rs
+++ api-sync/src/main.rs
@@ -20,7 +20,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::default());
         app
     })
-    .bind(("127.0.0.1", 8000))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
