aa628650395585646893f2a2416355ab45d0c970
Artem Yarulin <artem.yarulin@kapteko.com>
2023-09-15 09:27:35 +0300

    api-sync Get more info about possible error with git and vergen

--- api-sync/build.rs
+++ api-sync/build.rs
@@ -2,12 +2,12 @@ use std::error::Error;
 use vergen::EmitBuilder;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Emit the instructions
     EmitBuilder::builder()
         .all_build()
         .all_cargo()
         .all_git()
         .all_rustc()
+        .fail_on_error()
         .emit()?;
     Ok(())
 }
