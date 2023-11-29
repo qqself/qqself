c66542d66b23fc46713814d5de5b57154c295915
Artem Yarulin <artem.yarulin@kapteko.com>
2023-11-29 11:50:30 +0200

    client-ios First commit with Rust core integration working!

---
+++ client-ios/qqself/qqselfApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct qqselfApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ client-ios/qqselfCore.xcframework/ios-arm64_x86_64-simulator/qqselfCore.framework/Headers/Export.h
@@ -0,0 +1,1 @@
+#include "qqselfCoreFFI.h"
